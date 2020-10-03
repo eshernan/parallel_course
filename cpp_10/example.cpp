441d7f2ec666e3afe4a8077e9457d74d2a66b53c
Esteban Hernandez <eshernan@gmail.com>
2020-10-03 07:41:31 -0500

    Changes on c++20

--- cpp_10/example.cpp
+++ cpp_10/example.cpp
@@ -2,30 +2,30 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
- 
+
 struct Sum
 {
     void operator()(int n) { sum += n; }
     int sum{0};
 };
- 
+
 int main()
 {
     std::vector<int> nums{3, 4, 2, 8, 15, 267};
- 
-    auto print = [](const int& n) { std::cout << " " << n; };
- 
+
+    auto print = [](const int &n) { std::cout << " " << n; };
+
     std::cout << "before:";
     std::for_each(nums.cbegin(), nums.cend(), print);
     std::cout << '\n';
- 
-    std::for_each(nums.begin(), nums.end(), [](int &n){ n++; });
- 
-    // calls Sum::operator() for each number
-    //     Sum s = std::for_each(nums.begin(), nums.end(), Sum());
-    //      
-    //          std::cout << "after: ";
-    //              std::for_each(nums.cbegin(), nums.cend(), print);
-    //                  std::cout << '\n';
-    //                      std::cout << "sum: " << s.sum << '\n';
-    //                      }
+
+    std::for_each(nums.begin(), nums.end(), [](int &n) { n++; });
+
+     calls Sum::operator() for each number
+         Sum s = std::for_each(nums.begin(), nums.end(), Sum());
+
+     std::cout << "after: ";
+     std::for_each(nums.cbegin(), nums.cend(), print);
+     std::cout << '\n';
+     std::cout << "sum: " << s.sum << '\n';
+}
